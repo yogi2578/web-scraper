0c8ca82f87a8f9db2dd9c88e74dc0da8f8c59647
yogi2578 <ym2578@columbia.edu>
2016-12-15 20:39:06 -0500

    Add files via upload

--- nearestneighbor.py
+++ nearestneighbor.py
@@ -4,19 +4,29 @@ from nltk.tokenize import RegexpTokenizer
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.tokenize import word_tokenize
 from sklearn.model_selection import cross_val_score
 from sklearn.metrics import accuracy_score
 from nltk.corpus import stopwords
 import sys
+from nltk.stem.porter import PorterStemmer
+
+
 reload(sys)
 sys.setdefaultencoding('utf8')
 
 def preprocess_text(text):
+    porter_stemmer = PorterStemmer()
     stop = set(stopwords.words('english'))
-    tokens = word_tokenize(text)
-    cleanup = " ".join(filter(lambda word: word not in stop, text.split()))
-    return cleanup
+    cleanup = " ".join(filter(lambda word: word.lower() not in stop, text.split()))
+    cleanup = cleanup.encode('ascii','ignore');
+    text = text.encode('ascii', 'ignore');
+    stemmed = [porter_stemmer.stem(word) for word in text.lower().split()]
+    UNCOMMENT THIS LINE TO TRY STOP WORDS
+    # stemmed = [porter_stemmer.stem(word) for word in cleanup.lower().split()]
+    stem_clean = " ".join(stemmed)
+    return stem_clean
 
 knn = KNeighborsClassifier(n_neighbors=1)
 dataset=pd.read_csv('data.txt',delimiter='\t')
@@ -25,21 +35,20 @@ x=npm[:,0]    #sentence to classify
 q=[]#np.empty(176,dtype="string")
 i=0
 for line in x:
-    # q[i] = preprocess_text(line)
-    # q.append(line)
     q.append(preprocess_text(line))
     i+=1
 r= np.asarray(q)
 y=npm[:,2]   # output token_pattern=r'\b\w+\b',
-vectorizer=CountVectorizer(ngram_range=(2,2), min_df = 0.01, max_df = 0.99)
-unigram = vectorizer.fit_transform(x)
-trainx=unigram[0:120,:]
+# vectorizer=CountVectorizer(ngram_range=(2,2), min_df = 0.01, max_df = 0.99)
+tvectorizer=TfidfVectorizer(ngram_range=(2,2), min_df = 0.01, max_df = 0.99)
+bigram = tvectorizer.fit_transform(r)
+trainx=bigram[0:120,:]
 trainy=y[0:120]
-testx=unigram[120:176,:]
+testx=bigram[120:176,:]
 testy=y[120:176]
 knn.fit(trainx,trainy)
-pred = knn.predict(testx)
 print knn.score(testx,testy)
+# pred = knn.predict(testx)
 # print np.mean(testy == pred)
 # p=knn.predict(unigram)
 # scores= cross_val_score(knn, unigram, y, cv=5)
